97366e48cab35e80c60fb00adb58b2ba5e023a8d
Andrei <soprachev@mail.ru>
2022-04-22 22:13:00 +0300

    preview

---
+++ src/views/PathPreview.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>Preview</div>
+	<div>{{ data }}</div>
+</template>
+
+<script>
+import Axios from 'axios';
+export default {
+	data() {
+		return {
+			data: {},
+		};
+	},
+	methods: {},
+	computed: {},
+	async mounted() {
+		let id = this.$route.params.id;
+		if (id) {
+			let result = await Axios.get('https://polymap.ru/api/load/' + id);
+
+			this.data = result.data;
+		}
+	},
+};
+</script>
