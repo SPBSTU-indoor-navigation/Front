2ab9ea6744698eefb86dfba54ce54158b966e443
Andrei <soprachev@mail.ru>
2022-06-16 16:32:06 +0300

    upd

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,5 +3,34 @@
 </template>
 
 <script>
-export default {};
+export default {
+	metaInfo() {
+		return {
+			title: 'PolyMap',
+			meta: [
+				{
+					property: 'og:title',
+					content: `Карта кампуса СПбПУ`,
+				},
+				{
+					property: 'og:description',
+					content: 'Помещение и прилигающая территория',
+				},
+				{
+					property: 'og:url',
+					content: `polymap.ru${this.$route.fullPath}`,
+				},
+				{
+					property: 'og:image',
+					content: `${window.location.origin}/ogimage.jpg`,
+				},
+				{
+					name: 'apple-itunes-app',
+					content:
+						'app-id=1589702536, app-clip-bundle-id=com.soprachev.polymap.clip, app-clip-display=card',
+				},
+			],
+		};
+	},
+};
 </script>
--- src/views/PathPreview.vue
+++ src/views/PathPreview.vue
@@ -52,6 +52,11 @@ export default {
 					property: 'og:image',
 					content: `${window.location.origin}/ogimage.jpg`,
 				},
+				{
+					name: 'apple-itunes-app',
+					content:
+						'app-id=1589702536, app-clip-bundle-id=com.soprachev.polymap.clip, app-clip-display=card',
+				},
 			],
 		};
 	},
